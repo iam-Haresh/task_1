a5fd0be9949e72a555b13251aa7c7663e09a8a8a
Haresh Velusamy <hareshvelusamy@Hareshs-MacBook-Pro.local>
2025-01-02 12:07:55 +0530

    added logic to create ec2 machines in private subnet:

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 
 resource "aws_vpc" "task_vpc" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
 
   tags = {
--- variables.tf
+++ variables.tf
@@ -3,3 +3,7 @@ variable "upgrad_tag" {
   default = "upgrad_task"
 }
 
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
